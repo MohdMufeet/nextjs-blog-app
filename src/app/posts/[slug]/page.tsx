a97ca7ec04491105b9ad6d4467966080d76bbd85
MohdMufeet <mohdmufeet786@gmail.com>
2026-07-03 00:17:28 +0530

    feat: refactor PostPage to improve post data selection and layout

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -14,7 +14,8 @@ export default async function PostPage({ params }: Props) {
 
   const { data: post } = await supabase
     .from("posts")
-    .select("*")
+    // .select("*")
+    .select(`*,profiles (full_name),categories ( name)`)
     .eq("slug", slug)
     // .eq("status", true)
     .single();
@@ -25,17 +26,18 @@ export default async function PostPage({ params }: Props) {
 
   return (
     <main className="max-w-4xl mx-auto px-6 py-10">
-      <h1 className="text-4xl font-bold">
-        {post.title}
-      </h1>
+      <h1 className="text-4xl font-bold">{post.title}</h1>
 
       <p className="text-gray-500 mt-2">
         {new Date(post.created_at).toLocaleDateString()}
       </p>
+      <div className="flex gap-6 mt-4 text-gray-600">
+        <p>Author: {post.profiles?.full_name}</p>
 
-      <article className="prose max-w-none mt-8">
-        {post.content}
-      </article>
+        <p>Category: {post.categories?.name}</p>
+      </div>
+
+      <article className="prose max-w-none mt-8">{post.content}</article>
     </main>
   );
 }
